ebbb0f2e9dcee78791cb1f90e25e680985d965f4
gyanchandk <gyanchand1294@gmail.com>
2020-04-12 22:21:25 +0530

    hashing created

--- coprimeSubgroup.cpp
+++ coprimeSubgroup.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-#define SIZE 1000006
+#define SIZE 22
 int arr[SIZE];
 int set[SIZE];
 int count[SIZE];
@@ -15,6 +15,7 @@ void init()
   {
     arr[i]=-1;
     hashTable[i]=chainTable[i]=-1;
+    count[i]=0;
   }
 
   for(int i=2;i<SIZE/2;i++)
@@ -31,6 +32,8 @@ void init()
     }
   }
 
+  cout<<"\ninit done\n";
+
 }
 void assignSet()
 {
@@ -46,101 +49,104 @@ void assignSet()
       set[i]=set[i-arr[i]]+1;
     }
   }
+
+  cout<<"\nassign set done\n";
 }
 
 void createSet()
 {
-  int index=-1;
-  for(int i=1;i<=SIZE/2;i++)
-  {
-    //cout<<endl;
-    int tempCount=0;
-    for(int j=SIZE;j>=1;j--)
-    {
-      if(set[j]==i)
-      {
-        //cout<<j<<" ";
-        list[++index]=j;
-        tempCount++;
-      }
 
-    }
-    count[i]=tempCount;
+  for(int i=1;i<SIZE;i++)
+  {
+    //cout<<set[i]<<" ";
+    int h=set[i];
+    chainTable[i]=hashTable[h];
+    hashTable[h]=i;
+    count[set[i]]++;
+
   }
 
-
-
+  cout<<"\ncreate set done\n";
 }
 
 void print()
 {
-  for(int i=0;i<SIZE;i++)
+  for(int i=1;i<SIZE;i++)
   {
-    cout<<list[i]<<" ";
+    for(int j=hashTable[i];j!=-1;j=chainTable[j])
+    {
+      cout<<j<<" ";
+    }
+    //cout<<i<<":"<<set[i]<<" ";
+    cout<<endl;
   }
+
+
+
+
 }
 int main()
 {
   init();
   assignSet();
   createSet();
-  //print();
+  print();
   cout<<"done\n";
 
-  int t;
-  cin>>t;
-
-  while (t--)
-  {
-    int n;
-    cin>>n;
-
-    int totalCount=0;
-
-    while(true)
-    {
-      int start=0;
-      int end;
-      for(int i=1;i<=SIZE/2;i++)
-      {
-        end=start+count[i];
-        int localCount=0;
-        int flag=true;
-      //  cout<<"start:"<<start<<" end:"<<end<<endl;
-        for(int j=start;j<end;j++)
-        {
-          if(list[j]>n)
-          {
-            localCount++;
-          }
-          else
-          {
-
-
-            if(flag)
-            {
-              flag=false;
-              cout<<count[i]-localCount<<" ";
-
-            }
-            cout<<list[j]<<" ";
-            totalCount++;
-          }
-
-          if(totalCount==n)break;
-        }
-
-        if(totalCount==n)break;
-        cout<<endl;
-        start=end;
-      }
-
-      if(totalCount==n)break;
-
-    }
-
-    cout<<endl;
-  }
+  // int t;
+  // cin>>t;
+  //
+  // while (t--)
+  // {
+  //   int n;
+  //   cin>>n;
+  //
+  //   int totalCount=0;
+  //
+  //   while(true)
+  //   {
+  //     int start=0;
+  //     int end;
+  //     for(int i=1;i<=SIZE/2;i++)
+  //     {
+  //       end=start+count[i];
+  //       int localCount=0;
+  //       int flag=true;
+  //     //  cout<<"start:"<<start<<" end:"<<end<<endl;
+  //       for(int j=start;j<end;j++)
+  //       {
+  //         if(list[j]>n)
+  //         {
+  //           localCount++;
+  //         }
+  //         else
+  //         {
+  //
+  //
+  //           if(flag)
+  //           {
+  //             flag=false;
+  //             cout<<count[i]-localCount<<" ";
+  //
+  //           }
+  //           cout<<list[j]<<" ";
+  //           totalCount++;
+  //         }
+  //
+  //         if(totalCount==n)break;
+  //       }
+  //
+  //       if(totalCount==n)break;
+  //       cout<<endl;
+  //       start=end;
+  //     }
+  //
+  //     if(totalCount==n)break;
+  //
+  //   }
+  //
+  //   cout<<endl;
+  // }
 
   return 0;
 }
